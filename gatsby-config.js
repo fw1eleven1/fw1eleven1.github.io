826b4aee35fdb936db1be263352a6b2814e84320
jslee0510 <fw1eleven1@gmail.com>
2022-04-07 16:57:15 +0900

    CNAME 생성 플러그인 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,7 @@ module.exports = {
         policy: [{ userAgent: '*', allow: '/' }],
       },
     },
+    'gatsby-plugin-cname'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
